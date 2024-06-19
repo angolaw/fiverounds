c4b751b360b278fba7703ebc3c5c7a8afd2170eb
Willian S. <wsapcf@gmail.com>
2024-06-19 10:39:04 -0300

    chore: create userRepository

--- src/domain/student.ts
+++ src/domain/student.ts
@@ -4,10 +4,11 @@ class Student {
     email: string
     password: string
 
-    constructor({ id, name, email, password }) {
+    constructor({ id, name, email, password }: { id: string, name: string, email: string, password: string }) {
         this.id = id,
             this.name = name,
             this.email = email,
             this.password = password
     }
 }
+export default Student
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import Student from "./domain/student";
 
 const app = express();
 const port = process.env.PORT || 3000;
