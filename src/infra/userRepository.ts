18971f80c08223e87a12d7f6e983932ebcdd2f2d
Willian S. <wsapcf@gmail.com>
2024-06-25 19:31:48 -0300

    chore: add files to git

--- src/infra/userRepository.ts
+++ src/infra/userRepository.ts
@@ -1,3 +1,25 @@
-class UserRepository {
+class StudentRepository {
+async function create(studentData) {
+    const student = new StudentModel(studentData)
+    await student.save()
+    return new StudentRepository(student.toObject())
+}
+async function getById(studentId: string) {
+    const student = await StudentModel.findById(studentId)
+    return new Student(student.toObject())
 
+}
+async function update(studentId, studentData) {
+    const student = await StudentModel.findByIdAndUpdate(studentId, studentData, { new: true })
+    return new Student(Student.toObject())
+
+}
+async function delete (studentId) {
+    await StudentModel.findByIdAndDelete(studentId)
+
+}
+async function getAll() {
+    const Students = await StudentModel.find()
+    return Students.map(_ => new Student(_.toObject()))
+}
 }
