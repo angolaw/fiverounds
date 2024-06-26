4b56571f27269edc4bfcd82e11a180b86aeba9c7
williansantana.angola <williansantana.angola@gmail.com>
2024-06-26 12:37:09 +0000

    chore: add StudentRepository implementation

---
+++ src/infra/studentRepository.ts
@@ -0,0 +1,30 @@
+import knex from "knex";
+import Student from "../domain/student";
+
+class StudentRepository {
+    async create(studentData: Student) {
+        const  [id] = await knex('students').insert(studentData)
+        return new Student({id, ...studentData})
+
+    }
+    async getById(studentId: string){
+        const student = await knex('students').where({id: studentId}).first()
+        return student ? new Student(student) : null
+    }
+    async update(studentId: string, studentData:Student){
+        await knex('students').where({id: studentId}).update(studentData)
+        const updatedStudent = await this.getById(studentId)
+        return updatedStudent 
+    }
+    async delete(studentId: string){
+        await knex('students').where({id: studentId}).delete()
+
+    }
+    async getAll(){
+        const students = await knex('student').select()
+        return students.map(student => new Student(student))
+    }
+
+}
+module.exports = StudentRepository
+
